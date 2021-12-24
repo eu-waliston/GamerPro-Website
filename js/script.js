abc945517fcbe2eda1ba1178575466c502dc254a
Waliston E. Santos <82295321+bkt-tonton@users.noreply.github.com>
2021-12-24 14:00:53 -0300

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,36 @@
+const navItemDropdownList = document.querySelectorAll('.nav-item-dropdown');
+const navTogglerOpen = document.querySelector('.navbar-toggler-open');
+const navTogglerClose = document.querySelector('.navbar-toggler-close');
+const navbarCollapseDiv = document.querySelector('.navbar-collapse');
+
+navItemDropdownList.forEach((navItem) => {
+
+  navItem.addEventListener('click', () => {
+    navItem.nextElementSibling.classList.toggle('show-dropdown');
+
+
+    if (navItem.firstElementChild.className == "fas fa-chevron-down") {
+      navItem.firstElementChild.className = "fas fa-chevron-up";
+    } else if (navItem.firstElementChild.className == "fas fa-chevron-up") {
+      navItem.firstElementChild.className = "fas fa-chevron-down";
+    }
+  });
+});
+
+navTogglerOpen.addEventListener('click', () => {
+  navbarCollapseDiv.classList.add('show-navbar');
+});
+
+navTogglerClose.addEventListener('click', () => {
+  navbarCollapseDiv.classList.remove('show-navbar');
+});
+
+// animation and transition stopper 
+let resizeTimer;
+window.addEventListener('resize', () => {
+  document.body.classList.add('resize-animation-stopper');
+  clearTimeout(resizeTimer);
+  resizeTimer = setTimeout(() => {
+    document.body.classList.remove('resize-animation-stopper');
+  }, 400);
+});
